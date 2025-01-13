8eb34e143d28794b49bfea2a2497b248ceb76655
maurigvs <maurigvs@icloud.com>
2025-01-13 11:15:10 +0000

    crypto categories pageable

--- src/main/java/com/maurigvs/wallet/CryptoCriteriaDto.java
+++ src/main/java/com/maurigvs/wallet/CryptoCriteriaDto.java
@@ -3,6 +3,8 @@ package com.maurigvs.wallet;
 import java.util.List;
 
 public record CryptoCriteriaDto(
+        int page,
+        int size,
         List<CryptoFilterDto> filters,
         List<CryptoSortDto> sorts
 ) {
